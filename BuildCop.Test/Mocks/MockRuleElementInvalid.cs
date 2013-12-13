c58415c97a1d6aa38e078cb6c93f4d19c5c752a5
Ian Fraser <ian@bigwave.org.uk>
2013-12-13 23:41:00 +0000

    missing files

---
+++ BuildCop.Test/Mocks/MockRuleElementInvalid.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using BuildCop.Configuration;
+using BuildCop.Reporting;
+using BuildCop.Rules;
+
+namespace BuildCop.Test.Mocks
+{
+    internal class MockRuleElementInvalid : RuleConfigurationElement
+    {
+    }
+}
