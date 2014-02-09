fe1b41b3a7db18a0d80c748abb41580579c0e21f
Ian Fraser <ian@bigwave.org.uk>
2014-02-09 21:18:55 +0000

    fix more tests

--- BuildCop.Test/StrongNamingRuleTest.cs
+++ BuildCop.Test/StrongNamingRuleTest.cs
@@ -26,92 +26,93 @@ namespace BuildCop.Test
             Assert.IsNotNull(entries);
             Assert.AreEqual<int>(0, entries.Count);
         }
-        // TODO Fix these first
-    ////    [TestMethod]
-    ////    [DeploymentItem("BuildFiles", "BuildFiles")]
-    ////    public void VerifyUnsignedProjectThatShouldBeSigned()
-    ////    {
-    ////        BuildFile file = new BuildFile(@"BuildFiles\DefaultConsoleApplication.csproj");
-    ////        ruleElementStrongNaming config = new ruleElementStrongNaming();
-    ////        config.StrongNaming.KeyPath = "dummy";
-    ////        config.StrongNaming.StrongNameRequired = true;
-    ////        StrongNamingRule rule = new StrongNamingRule(config);
-    ////        rule.Name = "StrongNaming";
-    ////        IList<LogEntry> entries = rule.Check(file);
-    ////        Assert.IsNotNull(entries);
-    ////        Assert.AreEqual<int>(1, entries.Count);
-    ////        LogEntry entry = entries[0];
-    ////        Assert.AreEqual<LogLevel>(LogLevel.Error, entry.Level);
-    ////        Assert.AreEqual<string>("StrongNaming", entry.Rule);
-    ////        Assert.AreEqual<string>("SigningShouldBeEnabled", entry.Code);
-    ////    }
 
-    ////    [TestMethod]
-    ////    [DeploymentItem("BuildFiles", "BuildFiles")]
-    ////    public void VerifyIgnoredUnsignedProjectThatShouldBeSigned()
-    ////    {
-    ////        BuildFile file = new BuildFile(@"BuildFiles\DefaultConsoleApplication.csproj");
-    ////        ruleElementStrongNaming config = new ruleElementStrongNaming();
-    ////        config.StrongNaming.KeyPath = "dummy";
-    ////        config.StrongNaming.StrongNameRequired = true;
-    ////        config.StrongNaming.IgnoreUnsignedProjects = true;
-    ////        StrongNamingRule rule = new StrongNamingRule(config);
-    ////        rule.Name = "StrongNaming";
-    ////        IList<LogEntry> entries = rule.Check(file);
-    ////        Assert.IsNotNull(entries);
-    ////        Assert.AreEqual<int>(0, entries.Count);
-    ////    }
+		[TestMethod]
+		[DeploymentItem("BuildFiles", "BuildFiles")]
+		public void VerifyUnsignedProjectThatShouldBeSigned()
+		{
+			BuildFile file = new BuildFile(@"BuildFiles\DefaultConsoleApplication.csproj");
+			ruleElement rule = new ruleElement();
+			rule.strongNaming.strongNameRequired = false;
+			rule.name = "StrongNaming";
+			rule.RuleChecker = new StrongNamingRule(rule);
+			rule.strongNaming.strongNameRequired = true;
+			rule.name = "StrongNaming";
+			IList<LogEntry> entries = rule.RuleChecker.Check(file);
+			Assert.IsNotNull(entries);
+			Assert.AreEqual<int>(1, entries.Count);
+			LogEntry entry = entries[0];
+			Assert.AreEqual<LogLevel>(LogLevel.Error, entry.Level);
+			Assert.AreEqual<string>("StrongNaming", entry.Rule);
+			Assert.AreEqual<string>("SigningShouldBeEnabled", entry.Code);
+		}
 
-    ////    [TestMethod]
-    ////    [DeploymentItem("BuildFiles", "BuildFiles")]
-    ////    public void VerifySignedProjectThatShouldNotBeSigned()
-    ////    {
-    ////        BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
-    ////        ruleElementStrongNaming config = new ruleElementStrongNaming();
-    ////        config.StrongNaming.StrongNameRequired = false;
-    ////        StrongNamingRule rule = new StrongNamingRule(config);
-    ////        rule.Name = "StrongNaming";
-    ////        IList<LogEntry> entries = rule.Check(file);
-    ////        Assert.IsNotNull(entries);
-    ////        Assert.AreEqual<int>(1, entries.Count);
-    ////        LogEntry entry = entries[0];
-    ////        Assert.AreEqual<LogLevel>(LogLevel.Error, entry.Level);
-    ////        Assert.AreEqual<string>("StrongNaming", entry.Rule);
-    ////        Assert.AreEqual<string>("SigningShouldBeDisabled", entry.Code);
-    ////    }
+		[TestMethod]
+		[DeploymentItem("BuildFiles", "BuildFiles")]
+		public void VerifyIgnoredUnsignedProjectThatShouldBeSigned()
+		{
+			BuildFile file = new BuildFile(@"BuildFiles\DefaultConsoleApplication.csproj");
+			ruleElement rule = new ruleElement();
+			rule.strongNaming.keyPath = "dummy";
+			rule.strongNaming.strongNameRequired = true;
+			rule.strongNaming.ignoreUnsignedProjects = true;
+			rule.RuleChecker = new StrongNamingRule(rule);
+			rule.name = "StrongNaming";
+			IList<LogEntry> entries = rule.RuleChecker.Check(file);
+			Assert.IsNotNull(entries);
+			Assert.AreEqual<int>(0, entries.Count);
+		}
 
-    ////    [TestMethod]
-    ////    [DeploymentItem("BuildFiles", "BuildFiles")]
-    ////    public void VerifySignedProjectThatIsSignedWithIncorrectKey()
-    ////    {
-    ////        BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
-    ////        ruleElementStrongNaming config = new ruleElementStrongNaming();
-    ////        config.StrongNaming.KeyPath = "dummy";
-    ////        config.StrongNaming.StrongNameRequired = true;
-    ////        StrongNamingRule rule = new StrongNamingRule(config);
-    ////        rule.Name = "StrongNaming";
-    ////        IList<LogEntry> entries = rule.Check(file);
-    ////        Assert.IsNotNull(entries);
-    ////        Assert.AreEqual<int>(1, entries.Count);
-    ////        LogEntry entry = entries[0];
-    ////        Assert.AreEqual<LogLevel>(LogLevel.Error, entry.Level);
-    ////        Assert.AreEqual<string>("StrongNaming", entry.Rule);
-    ////        Assert.AreEqual<string>("SignedWithIncorrectKey", entry.Code);
-    ////    }
+		[TestMethod]
+		[DeploymentItem("BuildFiles", "BuildFiles")]
+		public void VerifySignedProjectThatShouldNotBeSigned()
+		{
+			BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
+			ruleElement rule = new ruleElement();
+			rule.strongNaming.strongNameRequired = false;
+			rule.RuleChecker = new StrongNamingRule(rule);
+			rule.name = "StrongNaming";
+			IList<LogEntry> entries = rule.RuleChecker.Check(file);
+			Assert.IsNotNull(entries);
+			Assert.AreEqual<int>(1, entries.Count);
+			LogEntry entry = entries[0];
+			Assert.AreEqual<LogLevel>(LogLevel.Error, entry.Level);
+			Assert.AreEqual<string>("StrongNaming", entry.Rule);
+			Assert.AreEqual<string>("SigningShouldBeDisabled", entry.Code);
+		}
 
-    ////    [TestMethod]
-    ////    [DeploymentItem("BuildFiles", "BuildFiles")]
-    ////    public void VerifySignedProjectThatIsSignedWithCorrectKey()
-    ////    {
-    ////        BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
-    ////        ruleElementStrongNaming config = new ruleElementStrongNaming();
-    ////        config.StrongNaming.KeyPath = @"C:\MyKey.snk";
-    ////        config.StrongNaming.StrongNameRequired = true;
-    ////        StrongNamingRule rule = new StrongNamingRule(config);
-    ////        rule.Name = "StrongNaming";
-    ////        IList<LogEntry> entries = rule.Check(file);
-    ////        Assert.IsNotNull(entries);
-    ////        Assert.AreEqual<int>(0, entries.Count);
-    ////    }
+		[TestMethod]
+		[DeploymentItem("BuildFiles", "BuildFiles")]
+		public void VerifySignedProjectThatIsSignedWithIncorrectKey()
+		{
+			BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
+			ruleElement rule = new ruleElement();
+			rule.strongNaming.keyPath = "dummy";
+			rule.strongNaming.strongNameRequired = true;
+			rule.RuleChecker = new StrongNamingRule(rule);
+			rule.name = "StrongNaming";
+			IList<LogEntry> entries = rule.RuleChecker.Check(file);
+			Assert.IsNotNull(entries);
+			Assert.AreEqual<int>(1, entries.Count);
+			LogEntry entry = entries[0];
+			Assert.AreEqual<LogLevel>(LogLevel.Error, entry.Level);
+			Assert.AreEqual<string>("StrongNaming", entry.Rule);
+			Assert.AreEqual<string>("SignedWithIncorrectKey", entry.Code);
+		}
+
+		[TestMethod]
+		[DeploymentItem("BuildFiles", "BuildFiles")]
+		public void VerifySignedProjectThatIsSignedWithCorrectKey()
+		{
+			BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
+			ruleElement rule = new ruleElement();
+			rule.strongNaming.keyPath = @"C:\MyKey.snk";
+			rule.strongNaming.strongNameRequired = true;
+			rule.RuleChecker = new StrongNamingRule(rule);
+			rule.name = "StrongNaming";
+			IList<LogEntry> entries = rule.RuleChecker.Check(file);
+			Assert.IsNotNull(entries);
+			Assert.AreEqual<int>(0, entries.Count);
+		}
     }
 }
