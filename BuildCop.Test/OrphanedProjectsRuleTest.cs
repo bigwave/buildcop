b063f266e48d232e9163b3e1eee7163bdce021f0
Ian Fraser <ian@bigwave.org.uk>
2014-02-09 21:45:34 +0000

    final tests fixed

--- BuildCop.Test/OrphanedProjectsRuleTest.cs
+++ BuildCop.Test/OrphanedProjectsRuleTest.cs
@@ -6,42 +6,43 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using BuildCop.Reporting;
 using BuildCop.Configuration;
+using BuildCop.Rules;
 
 namespace BuildCop.Test
 {
-    // TODO Fix these
-    ////[TestClass]
-    ////public class OrphanedProjectsRuleTest
-    ////{
-    ////    [TestMethod]
-    ////    [DeploymentItem("BuildFiles", "BuildFiles")]
-    ////    public void ProjectInSolutionShouldBeFound()
-    ////    {
-    ////        BuildFile file = new BuildFile(@"BuildFiles\DefaultConsoleApplication.csproj");
-    ////        OrphanedProjectsRuleElement config = new OrphanedProjectsRuleElement();
-    ////        config.Solutions.SearchPath = "BuildFiles";
-    ////        OrphanedProjectsRule rule = new OrphanedProjectsRule(config);
-    ////        rule.Name = "OrphanedProjects";
-    ////        IList<LogEntry> entries = rule.Check(file);
-    ////        Assert.IsNotNull(entries);
-    ////        Assert.AreEqual<int>(0, entries.Count);
-    ////    }
+	[TestClass]
+	public class OrphanedProjectsRuleTest
+	{
+		[TestMethod]
+		[DeploymentItem("BuildFiles", "BuildFiles")]
+		public void ProjectInSolutionShouldBeFound()
+		{
+			BuildFile file = new BuildFile(@"BuildFiles\DefaultConsoleApplication.csproj");
+			ruleElement rule = new ruleElement();
+			rule.solutions.searchPath = "BuildFiles";
+			rule.RuleChecker = new OrphanedProjectsRule(rule);
+			rule.name = "OrphanedProjects";
+			IList<LogEntry> entries = rule.RuleChecker.Check(file);
+			Assert.IsNotNull(entries);
+			Assert.AreEqual<int>(0, entries.Count);
+		}
 
-    ////    [TestMethod]
-    ////    [DeploymentItem("BuildFiles", "BuildFiles")]
-    ////    public void ProjectNotInSolutionShouldBeError()
-    ////    {
-    ////        BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
-    ////        OrphanedProjectsRuleElement config = new OrphanedProjectsRuleElement();
-    ////        config.Solutions.SearchPath = "BuildFiles";
-    ////        OrphanedProjectsRule rule = new OrphanedProjectsRule(config);
-    ////        rule.Name = "OrphanedProjects";
-    ////        IList<LogEntry> entries = rule.Check(file);
-    ////        Assert.IsNotNull(entries);
-    ////        Assert.AreEqual<int>(1, entries.Count);
-    ////        LogEntry entry = entries[0];
-    ////        Assert.AreEqual<LogLevel>(LogLevel.Error, entry.Level);
-    ////        Assert.AreEqual<string>("OrphanedProjects", entry.Rule);
-    ////        Assert.AreEqual<string>("OrphanedProject", entry.Code);
-    ////    }
-    }
+		[TestMethod]
+		[DeploymentItem("BuildFiles", "BuildFiles")]
+		public void ProjectNotInSolutionShouldBeError()
+		{
+			BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
+			ruleElement rule = new ruleElement();
+			rule.solutions.searchPath = "BuildFiles";
+			rule.RuleChecker = new OrphanedProjectsRule(rule);
+			rule.name = "OrphanedProjects";
+			IList<LogEntry> entries = rule.RuleChecker.Check(file);
+			Assert.IsNotNull(entries);
+			Assert.AreEqual<int>(1, entries.Count);
+			LogEntry entry = entries[0];
+			Assert.AreEqual<LogLevel>(LogLevel.Error, entry.Level);
+			Assert.AreEqual<string>("OrphanedProjects", entry.Rule);
+			Assert.AreEqual<string>("OrphanedProject", entry.Code);
+		}
+	}
+}
