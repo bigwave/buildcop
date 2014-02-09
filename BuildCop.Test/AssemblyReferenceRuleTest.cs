830a7f438127d24f62ce89690e1366b4aa7aa94c
Ian Fraser <ian@bigwave.org.uk>
2014-02-09 21:34:52 +0000

    more tests fixed

--- BuildCop.Test/AssemblyReferenceRuleTest.cs
+++ BuildCop.Test/AssemblyReferenceRuleTest.cs
@@ -13,81 +13,81 @@ namespace BuildCop.Test
     [TestClass]
     public class AssemblyReferenceRuleTest
     {
-        // TODO fix these
-        ////[TestMethod]
-        ////[DeploymentItem("BuildFiles", "BuildFiles")]
-        ////public void VerifyCorrectAssemblyReferences()
-        ////{
-        ////    BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
-        ////    AssemblyReferenceRuleElement config = new AssemblyReferenceRuleElement();
-        ////    AssemblyLocationElement asmLocation = new AssemblyLocationElement();
-        ////    asmLocation.AssemblyName = "EnvDTE";
-        ////    asmLocation.AssemblyPath = @"X:\References\Microsoft.Practices.RecipeFramework\1.0.60429.0\EnvDTE.dll";
-        ////    config.AssemblyLocations.Add(asmLocation);
-        ////    AssemblyReferenceRule rule = new AssemblyReferenceRule(config);
-        ////    rule.Name = "AssemblyReference";
-        ////    IList<LogEntry> entries = rule.Check(file);
-        ////    Assert.IsNotNull(entries);
-        ////    Assert.AreEqual<int>(0, entries.Count);
-        ////}
+		[TestMethod]
+		[DeploymentItem("BuildFiles", "BuildFiles")]
+		public void VerifyCorrectAssemblyReferences()
+		{
+			BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
+			ruleElement rule = new ruleElement();
+			ruleElementAssemblyLocation asmLocation = new ruleElementAssemblyLocation();
+			asmLocation.assemblyName = "EnvDTE";
+			asmLocation.assemblyPath = @"X:\References\Microsoft.Practices.RecipeFramework\1.0.60429.0\EnvDTE.dll";
+			rule.assemblyLocations.Add(asmLocation);
+			rule.RuleChecker = new AssemblyReferenceRule(rule);
+			rule.name = "AssemblyReference";
+			IList<LogEntry> entries = rule.RuleChecker.Check(file);
+			Assert.IsNotNull(entries);
+			Assert.AreEqual<int>(0, entries.Count);
+		}
 
-        ////[TestMethod]
-        ////[DeploymentItem("BuildFiles", "BuildFiles")]
-        ////public void VerifyIncorrectAssemblyReferences()
-        ////{
-        ////    BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
-        ////    AssemblyReferenceRuleElement config = new AssemblyReferenceRuleElement();
-        ////    AssemblyLocationElement asmLocation = new AssemblyLocationElement();
-        ////    asmLocation.AssemblyName = "EnvDTE";
-        ////    asmLocation.AssemblyPath = "dummy";
-        ////    config.AssemblyLocations.Add(asmLocation);
-        ////    AssemblyReferenceRule rule = new AssemblyReferenceRule(config);
-        ////    rule.Name = "AssemblyReference";
-        ////    IList<LogEntry> entries = rule.Check(file);
-        ////    Assert.IsNotNull(entries);
-        ////    Assert.AreEqual<int>(1, entries.Count);
-        ////    LogEntry entry = entries[0];
-        ////    Assert.AreEqual<LogLevel>(LogLevel.Error, entry.Level);
-        ////    Assert.AreEqual<string>("AssemblyReference", entry.Rule);
-        ////    Assert.AreEqual<string>("IncorrectHintPath", entry.Code);
-        ////}
+		[TestMethod]
+		[DeploymentItem("BuildFiles", "BuildFiles")]
+		public void VerifyIncorrectAssemblyReferences()
+		{
+			BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
+			ruleElement rule = new ruleElement();
+			ruleElementAssemblyLocation asmLocation = new ruleElementAssemblyLocation();
+			asmLocation.assemblyName = "EnvDTE";
+			asmLocation.assemblyPath = "dummy";
+			rule.assemblyLocations.Add(asmLocation);
+			rule.RuleChecker = new AssemblyReferenceRule(rule);
+			rule.name = "AssemblyReference";
+			IList<LogEntry> entries = rule.RuleChecker.Check(file);
+			Assert.IsNotNull(entries);
+			Assert.AreEqual<int>(1, entries.Count);
+			LogEntry entry = entries[0];
+			Assert.AreEqual<LogLevel>(LogLevel.Error, entry.Level);
+			Assert.AreEqual<string>("AssemblyReference", entry.Rule);
+			Assert.AreEqual<string>("IncorrectHintPath", entry.Code);
+		}
 
-        ////[TestMethod]
-        ////[DeploymentItem("BuildFiles", "BuildFiles")]
-        ////public void VerifyCorrectAssemblyReferencesBecauseOfMissingAssemblyPath()
-        ////{
-        ////    BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
-        ////    AssemblyReferenceRuleElement config = new AssemblyReferenceRuleElement();
-        ////    AssemblyLocationElement asmLocation = new AssemblyLocationElement();
-        ////    asmLocation.AssemblyName = "dummy";
-        ////    asmLocation.AssemblyPath = "";
-        ////    config.AssemblyLocations.Add(asmLocation);
-        ////    asmLocation = new AssemblyLocationElement();
-        ////    asmLocation.AssemblyName = "EnvDTE";
-        ////    asmLocation.AssemblyPath = "";
-        ////    config.AssemblyLocations.Add(asmLocation);
-        ////    AssemblyReferenceRule rule = new AssemblyReferenceRule(config);
-        ////    rule.Name = "AssemblyReference";
-        ////    IList<LogEntry> entries = rule.Check(file);
-        ////    Assert.IsNotNull(entries);
-        ////    Assert.AreEqual<int>(0, entries.Count);
-        ////}
+		[TestMethod]
+		[DeploymentItem("BuildFiles", "BuildFiles")]
+		public void VerifyCorrectAssemblyReferencesBecauseOfMissingAssemblyPath()
+		{
+			BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
+			ruleElement rule = new ruleElement();
+			ruleElementAssemblyLocation asmLocation = new ruleElementAssemblyLocation();
+			asmLocation.assemblyName = "dummy";
+			asmLocation.assemblyPath = "";
+			rule.assemblyLocations.Add(asmLocation);
+			asmLocation = new ruleElementAssemblyLocation();
+			asmLocation.assemblyName = "EnvDTE";
+			asmLocation.assemblyPath = "";
+			rule.assemblyLocations.Add(asmLocation);
+			rule.RuleChecker = new AssemblyReferenceRule(rule);
+			rule.name = "AssemblyReference";
+			IList<LogEntry> entries = rule.RuleChecker.Check(file);
+			Assert.IsNotNull(entries);
+			Assert.AreEqual<int>(0, entries.Count);
+		}
 
-        ////[TestMethod]
-        ////[DeploymentItem("BuildFiles", "BuildFiles")]
-        ////public void VerifyMissingAssemblyLocation()
-        ////{
-        ////    BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
-        ////    AssemblyReferenceRuleElement config = new AssemblyReferenceRuleElement();
-        ////    AssemblyReferenceRule rule = new AssemblyReferenceRule(config);
-        ////    rule.Name = "AssemblyReference";
-        ////    IList<LogEntry> entries = rule.Check(file);
-        ////    Assert.IsNotNull(entries);
-        ////    Assert.AreEqual<int>(1, entries.Count);
-        ////    LogEntry entry = entries[0];
-        ////    Assert.AreEqual<LogLevel>(LogLevel.Warning, entry.Level);
-        ////    Assert.AreEqual<string>("AssemblyReference", entry.Rule);
-        ////    Assert.AreEqual<string>("MissingAssemblyLocation", entry.Code);
-        ////}
+		[TestMethod]
+		[DeploymentItem("BuildFiles", "BuildFiles")]
+		public void VerifyMissingAssemblyLocation()
+		{
+			BuildFile file = new BuildFile(@"BuildFiles\SignedConsoleApplication.csproj");
+			ruleElement rule = new ruleElement();
+			ruleElementAssemblyLocation asmLocation = new ruleElementAssemblyLocation();
+			rule.name = "AssemblyReference";
+			rule.RuleChecker = new AssemblyReferenceRule(rule);
+			IList<LogEntry> entries = rule.RuleChecker.Check(file);
+			Assert.IsNotNull(entries);
+			Assert.AreEqual<int>(1, entries.Count);
+			LogEntry entry = entries[0];
+			Assert.AreEqual<LogLevel>(LogLevel.Warning, entry.Level);
+			Assert.AreEqual<string>("AssemblyReference", entry.Rule);
+			Assert.AreEqual<string>("MissingAssemblyLocation", entry.Code);
+		}
     }
 }
